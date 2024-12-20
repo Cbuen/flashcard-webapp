a952ba65508113e8b8b199cb3998d955fe0653e2
travel_top <chrismightdev@gmail.com>
2024-12-20 11:30:29 -0800

    Finish card list functionality, add some type way to open this view

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.querySelector(".flip-card").addEventListener("click", function () {
+  this.classList.toggle("flipped");
+});
+
+// test code
+let w1 = "hola";
+let w2 = "nombre";
+let w3 = "como estas";
+const words = [w1, w2, w3];
