288b62468421b4779a09be5aaf3e22d9287ade11
travel_top <chrismightdev@gmail.com>
2024-12-20 16:32:43 -0800

    Need to add more js functionality, add front end button functionality

--- scripts.js
+++ scripts.js
@@ -9,3 +9,15 @@ let w3 = "como estas";
 const words = [w1, w2, w3];
 
 // card-list-button
+const card_button = document.getElementById("card-list-button");
+const card_items = document.getElementsByClassName("card-item");
+
+card_button.addEventListener("click", function () {
+  for (let i = 0; i < card_items.length; i++) {
+    if (card_items[i].classList.contains("hidden")) {
+      card_items[i].classList.remove("hidden");
+    } else {
+      card_items[i].classList.add("hidden");
+    }
+  }
+});
