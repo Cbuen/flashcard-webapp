96d789cb4f32e52ffb5f9145d6fc677e8ed01695
travel_top <chrismightdev@gmail.com>
2024-12-30 20:30:36 -0800

    Study page done in terms of functionality, TODO: Organize and comment code readability, add ability to add more cards to the set

--- flashcard_app/members/views.py
+++ flashcard_app/members/views.py
@@ -29,8 +29,36 @@ def success_page(request):
 
 
 def study(request):
+    user_setid = 1
+    user_selected_set = "Select A Set"
     card_sets = Sets.objects.all()
-    card_set = Cards.objects.filter(setid=1)
+    card_set = Cards.objects.filter(setid=user_setid)
+
+    if request.method == "POST":
+        user_selected_set = request.POST.get("selected_card")
+        user_selected_set = user_selected_set.split("|")
+
+    if (
+        isinstance(user_selected_set, str)
+        and user_selected_set.lower() == "select a set"
+    ):
+        return render(
+            request,
+            "study.html",
+            {
+                "card_set": card_set,
+                "card_set_list": card_sets,
+                "user_selected_set_name": user_selected_set,
+            },
+        )
+    card_set = Cards.objects.filter(setid=user_selected_set[1])
     return render(
-        request, "study.html", {"card_set": card_set, "card_set_list": card_sets}
+        request,
+        "study.html",
+        {
+            "card_set": card_set,
+            "card_set_list": card_sets,
+            "user_selected_set_name": user_selected_set[0],
+            "user_selected_set_id": user_selected_set[1],
+        },
     )
