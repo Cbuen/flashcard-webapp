3e5c27f60baf06ec10c54638d1c0f29942b75656
Cbuen <chrismightdev@gmail.com>
2025-01-01 15:04:50 -0800

    Basic layout of edit page TODO: Finish functionality  of editor and be able to update the selected card set

--- flashcard_app/flashcard_app/static/dialog-edit.js
+++ flashcard_app/flashcard_app/static/dialog-edit.js
@@ -1,3 +1,13 @@
-function buttonTest(button_id) {
-  console.log(button_id);
+function buttonTest(button) {
+  const term = button.getAttribute('data-term');
+  const definition = button.getAttribute('data-definitions');
+
+  console.log(button.id);
+  console.log(term);
+  console.log(definition);
+}
+
+function clearInput() {
+  document.querySelectorAll("input.clear-input").forEach(input => input.value = '');
+
 }
