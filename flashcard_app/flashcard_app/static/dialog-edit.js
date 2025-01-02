7ac097c3d6e647dabe604cf9ce3f420fae817221
Cbuen <chrismightdev@gmail.com>
2025-01-02 11:08:37 -0800

    Added Set ID Labels on Edit page TODO: Be able to add more sets, add some UI/UX animations for closing edit boxes (clean workflow)

--- flashcard_app/flashcard_app/static/dialog-edit.js
+++ flashcard_app/flashcard_app/static/dialog-edit.js
@@ -1,16 +1,16 @@
-  function userSet(button) {
-    const term = button.getAttribute('data-term');
-    const definition = button.getAttribute('data-definition');
-    const selected_set = button.getAttribute('data-selectedset');
-    const input_fields = document.querySelectorAll("input.clear-input");
+function userSet(button) {
+  const term = button.getAttribute('data-term');
+  const definition = button.getAttribute('data-definition');
+  const selected_set = button.getAttribute('data-selectedset');
+  const input_fields = document.querySelectorAll("input.clear-input");
 
-    input_fields[0].value = term;
-    input_fields[0].id = button.id;
-    input_fields[1].value = definition;
-    input_fields[2].value = selected_set;
-  }
+  input_fields[0].value = term;
+  input_fields[0].id = button.id;
+  input_fields[1].value = definition;
+  input_fields[2].value = selected_set;
+}
 
-  function clearInput() {
-    document.querySelectorAll("input.clear-input").forEach(input => input.value = '');
+function clearInput() {
+  document.querySelectorAll("input.clear-input").forEach(input => input.value = '');
 
-  }
+}
